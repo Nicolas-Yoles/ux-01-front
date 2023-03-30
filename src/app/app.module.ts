7b0692ad347199d007a4ce5a762427d60d3fd897
Nicolas-Yoles <nicoyoles@gmail.com>
2023-03-30 01:05:53 -0300

    landing page initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LandingPageComponent } from './modulos/landing-page/landing-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
     LandingPageComponent
   ],
   imports: [
@@ -17,6 +15,6 @@ import { LandingPageComponent } from './modulos/landing-page/landing-page.compon
     BrowserAnimationsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [LandingPageComponent]
 })
 export class AppModule { }
