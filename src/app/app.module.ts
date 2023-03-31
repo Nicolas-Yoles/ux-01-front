c5b56f4316944e93c521e877215329c41d9be3e7
Nicolas-Yoles <nicoyoles@gmail.com>
2023-03-31 19:35:55 -0300

    display commit partial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LandingPageComponent } from './modulos/landing-page/landing-page.component';
+import { GenericCardComponent } from './utilities/generic-card/generic-card.component';
 
 @NgModule({
   declarations: [
-    LandingPageComponent
+    LandingPageComponent,
+    GenericCardComponent
   ],
   imports: [
     BrowserModule,
