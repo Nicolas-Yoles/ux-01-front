51b5e11ff7593c590a32d17b86a053926eed3690
Nicolas-Yoles <nicoyoles@gmail.com>
2023-04-03 18:38:05 -0300

    formulario initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LandingPageComponent } from './modulos/landing-page/landing-page.component';
 import { GenericCardComponent } from './utilities/generic-card/generic-card.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { CardFormComponent } from './modulos/card/card-form/card-form.component';
+import { CardListComponent } from './modulos/card/card-list/card-list.component';
 
 @NgModule({
-  declarations: [
-    LandingPageComponent,
-    GenericCardComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule
-  ],
+  declarations: [LandingPageComponent, GenericCardComponent, CardFormComponent, CardListComponent],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, MatCardModule, MatButtonModule],
   providers: [],
-  bootstrap: [LandingPageComponent]
+  bootstrap: [LandingPageComponent],
 })
-export class AppModule { }
+export class AppModule {}
